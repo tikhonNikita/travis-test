0611f4231b865e1192a563be78c3abdbe380da6b
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-10-25 15:55:49 +0300

    update travis config

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -9,16 +9,16 @@ module.exports = {
 
   development: {
     database: 'books',
-    username: 'postgres',
-    password: 'postgres',
+    username: 'test',
+    password: 'test',
     host: '127.0.0.1',
     dialect: 'postgres'
   },
 
   test: {
     database: 'book_test',
-    username: 'postgres',
-    password: 'postgres',
+    username: 'test',
+    password: 'test',
     host: '127.0.0.1',
     dialect: 'postgres'
   },
